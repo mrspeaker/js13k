a36a3f1e6e318c92cdc27035d31c2a7b1f5c94a0
Mr Speaker <mrspeaker@gmail.com>
2013-09-12 07:16:41 -0400

    something of a title screen

--- src/scripts/screens/title.js
+++ src/scripts/screens/title.js
@@ -4,6 +4,7 @@ Screen.title = {
 	count: 0,
 
 	init: function () {
+		this.tiles = makeSheet(game.res.tiles, game.tw, game.th);
 		return this;
 	},
 
@@ -18,10 +19,24 @@ Screen.title = {
 	},
 
 	render: function (c) {
-		c.fillStyle = "#000";
-		c.fillRect(0, 0, c.w, c.h);
 
-		game.res.font(c, "JS13K BY MRSPEAKER", 140, 160);
+		c.clearRect(0, 0, c.w, c.h);
+		c.save();
+		c.scale(3, 3);
+
+		for (var i = 0; i < 12; i++) {
+			this.tiles.render(c, 5, 0, i * game.tw, 100);
+			for (var j = 0; j < 5; j++) {
+				this.tiles.render(c, 7, 0, i * game.tw, game.th * (j + 1) + 100);
+			}
+		}
+
+		game.res.font(c, "JS13K", 15 + Math.sin(Date.now() / 300) * 5, 10 + Math.cos(Date.now() / 200) * 2);
+		game.res.font(c, "BY", 10, 40);
+		game.res.font(c, "MR SPEAKER", 10, 70);
+
+
+		c.restore();
 	}
 
 };
