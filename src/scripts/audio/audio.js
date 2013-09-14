01adffefe3a20411409b85104d8d024b1ce5cf4f
Mr Speaker <mrspeaker@gmail.com>
2013-09-14 12:52:24 -0400

    refactory audio functions

--- src/scripts/audio/audio.js
+++ src/scripts/audio/audio.js
@@ -3,15 +3,30 @@
 	var c;
 
 	function envelope(gain, time, volume, duration, a, d, s, r) {
-        gain.gain.cancelScheduledValues(0);
-        gain.gain.setValueAtTime(0, time);
-        gain.gain.linearRampToValueAtTime(volume, time + a);
-        gain.gain.linearRampToValueAtTime(volume * s, time + a + d);
-        gain.gain.setValueAtTime(volume * s, time + a + d + duration);
-        gain.gain.linearRampToValueAtTime(0, time + a + d + duration + r);
+		var g = gain.gain;
+        g.cancelScheduledValues(0);
+        g.setValueAtTime(0, time);
+        g.linearRampToValueAtTime(volume, time + a);
+        g.linearRampToValueAtTime(volume * s, time + a + d);
+        g.setValueAtTime(volume * s, time + a + d + duration);
+        g.linearRampToValueAtTime(0, time + a + d + duration + r);
     }
 
-	function noise () {
+    function gainWithFilter(input, gain, freq, q) {
+		var f = c.createBiquadFilter(),
+			g = createGain();
+
+		input.connect(f);
+		f.connect(g);
+
+		g.gain.value = gain;
+		f.frequency.value = freq;
+		f.Q.value = q;
+
+		return [g, f];
+    }
+
+	function createNoise () {
 		var buf = c.createBuffer(1, (60 / 120) * c.sampleRate, c.sampleRate),
 			data = buf.getChannelData(0),
 			node;
@@ -26,124 +41,100 @@
 		return node;
 	}
 
+	function createGain() {
+		var node;
+		if (c.createGain) { node = c.createGain(); }
+		else if (c.createGainNode) { node = c.createGainNode(); }
+		else { audio.ctx = c = null; }
+		return node;
+	}
+
 	window.audio = {
 
-		jumpedat: Date.now(),
+		jumpedAt: Date.now(),
 
 		sfx: {
-			"jump": function() {
-				if(!c) return;
-
-				if (Date.now() - audio.jumpedat < 100) {
+			jump: function() {
+				if (!c || Date.now() - audio.jumpedAt < 100) {
 					return;
 				}
-				audio.jumpedat = Date.now();
+				audio.jumpedAt = Date.now();
 
-				var now = c.currentTime;
-				var o = c.createOscillator();
-				var f = c.createBiquadFilter();
-				var g = audio.createGain();
-				o.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
-
-				g.gain.value = 0.35;
-				f.frequency.value = 2000;
-				f.Q.value = 3;
+				var now = c.currentTime,
+					o = c.createOscillator();
 
 				o.type = "sine"
 				o.frequency.value = 0;
 				o.frequency.setValueAtTime(300, now);
 				o.frequency.linearRampToValueAtTime(400, now + 0.09);
 
+				gainWithFilter(o, 0.35, 2000, 3)[0].connect(audio.master);
+
 				audio.start(o, now);
 				audio.stop(o, now + 0.1);
 			},
 
 			shoot: function () {
-				if(!c) return;
-				var now = c.currentTime;
-				var s = audio.noise;
-				var f = c.createBiquadFilter();
-				var g = audio.createGain();
+				if (!c) return;
+				var now = c.currentTime,
+					start = Math.random() * 2000 + 500 | 0,
+					s = audio.noise,
+					gf;
 
-				envelope(g, now, 0.7, 0.04, 0.01, 0.01, 0.1, 0.1);
-
-				var start = Math.random() * 2000 + 500 | 0;
-				f.Q.value = 10;
-				f.frequency.value = start;
-				f.frequency.setValueAtTime(start, now);
-				f.frequency.linearRampToValueAtTime(6000, now + 0.04);
-
-				s.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
+				gf = gainWithFilter(s, 1, start, 10);
+				gf[1].frequency.setValueAtTime(start, now);
+				gf[1].frequency.linearRampToValueAtTime(6000, now + 0.04);
+				envelope(gf[0], now, 0.7, 0.04, 0.01, 0.01, 0.1, 0.1);
+				gf[0].connect(audio.master);
 
 			},
 
 			pickup: function () {
-				if(!c) return;
+				if (!c) return;
 				var now = c.currentTime,
-					o = c.createOscillator(),
-					f = c.createBiquadFilter(),
-					g = audio.createGain();
-				o.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
-
-				g.gain.value = 0.12;
-				f.frequency.value = 3000;
-				f.Q.value = 10;
+					o = c.createOscillator();
 
 				o.type = "sine"
 				o.frequency.value = 0;
 				o.frequency.setValueAtTime(600, now);
 				o.frequency.linearRampToValueAtTime(2600, now + 0.12);
 
+				gainWithFilter(o, 0.12, 3000, 10)[0].connect(audio.master);
+
 				audio.start(o, 0);
 				audio.stop(o, now + 0.12);
 			},
 
 			collect: function () {
-				if(!c) return;
+				if (!c) return;
 				var now = c.currentTime;
 				var o = c.createOscillator(),
 					lfo = c.createOscillator(),
-					lfogain = audio.createGain();
-				var f = c.createBiquadFilter();
-				var g = audio.createGain();
+					lfoGain = createGain();
+
+				gainWithFilter(o, 1, 350, 1)[0].connect(audio.master);
 
 				o.type = "sine"
-				o.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
 
 				lfo.type = "sine";
 				lfo.frequency.value = 10;
-				lfogain.gain.setValueAtTime(10, now);
-				lfogain.gain.linearRampToValueAtTime(200, now + 1);
-				lfo.connect(lfogain);
-				lfogain.connect(o.frequency);
+				lfoGain.gain.setValueAtTime(10, now);
+				lfoGain.gain.linearRampToValueAtTime(200, now + 1);
+				lfo.connect(lfoGain);
+				lfoGain.connect(o.frequency);
 
-				g.gain.value = 1;
 				audio.start(lfo, now);
 				audio.start(o, now);
+				audio.stop(lfo, now + 1);
 				audio.stop(o, now + 1);
 			},
 
 			swiggle: function () {
-				if(!c) return;
-				var now = c.currentTime;
-				var o = c.createOscillator();
-				var f = c.createBiquadFilter();
-				var g = audio.createGain();
-				o.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
+				if (!c) return;
+				var now = c.currentTime,
+					o = c.createOscillator();
 
-				g.gain.value = 0.10;
-				f.frequency.value = 3000;
-				f.Q.value = 10;
+				gainWithFilter(o, 0.10, 3000, 10)[0].connect(audio.master);
 
 				o.type = "square"
 				o.frequency.value = 0;
@@ -155,24 +146,18 @@
 			},
 
 			die: function () {
-				if(!c) return;
+				if (!c) return;
 				var now = c.currentTime,
 					s = audio.noise,
-					f = c.createBiquadFilter(),
-					g = audio.createGain();
+					start = Math.random() * 20000 + 500 | 0,
+					gf;
 
-				envelope(g, now, 0.6, 0.5, 0.01, 0.01, 0.4, 0.08);
+				gf = gainWithFilter(s, 1, start, 10);
+				envelope(gf[0], now, 0.6, 0.5, 0.01, 0.01, 0.4, 0.08);
 
-				var start = Math.random() * 20000 + 500 | 0;
-				f.Q.value = 10;
-				f.frequency.value = start;
-				f.frequency.setValueAtTime(start, now);
-				f.frequency.linearRampToValueAtTime(200, now + 0.5);
-
-				s.connect(f);
-				f.connect(g);
-				g.connect(audio.master);
-
+				gf[1].frequency.setValueAtTime(start, now);
+				gf[1].frequency.linearRampToValueAtTime(200, now + 0.5);
+				gf[0].connect(audio.master);
 			}
 		},
 
@@ -184,13 +169,6 @@
 				c = new webkitAudioContext();
 			}
 
-			this.createGain = function () {
-				var node = null;
-				if (c.createGain) { node = c.createGain(); }
-				else if (c.createGainNode) { node = c.createGainNode(); }
-				else { this.ctx = c = null; }
-				return node
-			}
 			this.start = function (node, time) {
 				if (node.start) { node.start(time); }
 				else if (node.noteOn) { node.noteOn(time); }
@@ -202,7 +180,7 @@
 				else { this.ctx = c = null; }
 			}
 
-			if (c && (!this.createGain() || !c.createOscillator)) {
+			if (c && (!createGain() || !c.createOscillator)) {
 				c = null;
 			}
 
@@ -211,20 +189,16 @@
 				return;
 			}
 
-			this.noise = noise();
+			this.noise = createNoise();
 			this.start(this.noise, 0);
 
-			this.master = audio.createGain();
+			this.master = createGain();
 			this.master.gain.value = 1;
 			this.master.connect(c.destination);
 
-		},
-
-		stop: function () {
-			this.osc.stop(c.currentTime);
 		}
 
-};
+	};
 
 }());
 
